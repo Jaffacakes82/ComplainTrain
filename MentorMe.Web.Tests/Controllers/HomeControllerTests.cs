02ec68c17dfbe17e13f7850ecf4e0023a70e11ac
Joe Ellis <joe.ellis@avcosystems.com>
2016-12-13 22:56:27 +0000

    Implemented moq and xunit, wrote a test

---
+++ MentorMe.Web.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using MentorMe.Core.Settings;
+using MentorMe.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Moq;
+using Xunit;
+
+namespace MentorMe.Web.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        private readonly Mock<IOptions<MessageSettings>> messageSettings;
+
+        public HomeControllerTests()
+        {
+            this.messageSettings = new Mock<IOptions<MessageSettings>>();
+        }
+
+        [Fact]
+        public void HomeController_GET_Index_ReturnsIndexView() 
+        {
+            // Arrange
+            MessageSettings testSettings = new MessageSettings { MessageOfTheDay = "Test" };
+            this.messageSettings.SetupGet(msg => msg.Value).Returns(testSettings);
+
+            // Act
+            HomeController homeController = new HomeController(this.messageSettings.Object);
+            ViewResult result = (ViewResult)homeController.Index();
+            
+            // Assert
+            Assert.Equal("Index", result.ViewName);
+        }
+    }
+}
--- MentorMe.Web/Controllers/HomeController.cs
+++ MentorMe.Web/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using MentorMe.Core.Settings;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
@@ -14,10 +15,10 @@ namespace MentorMe.Web.Controllers
         }
 
         [HttpGet]
-        public IActionResult Index()
+        public ActionResult Index()
         {
             this.ViewData["Message"] = this.messageOptions.MessageOfTheDay;
-            return this.View();
+            return this.View("Index");
         }
     }
 }
