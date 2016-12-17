12560ad222c9a6082dcd85522c3a7f3ccee0a997
Joe Ellis <joe.ellis@avcosystems.com>
2016-12-17 14:57:52 +0000

    Seeing if failed test breaks build

--- TrainComplain.Web.Tests/Controllers/HomeControllerTests.cs
+++ TrainComplain.Web.Tests/Controllers/HomeControllerTests.cs
@@ -17,6 +17,12 @@ namespace TrainComplain.Web.Tests.Controllers
         }
 
         [Fact]
+        public void FailedTestStub()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
         public void HomeController_GET_Index_ReturnsIndexView() 
         {
             // Arrange
