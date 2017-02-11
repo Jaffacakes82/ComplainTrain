69c99164548e19b107a92683c5ca1317cccfd5b0
Joe Ellis <joe.ellis@avcosystems.com>
2017-02-11 22:39:39 +0000

    Added commit message, finalised panels

--- ComplainTrain.Web/wwwroot/js/site.js
+++ ComplainTrain.Web/wwwroot/js/site.js
@@ -1,3 +1,33 @@
+var departureBoardHtml = '<div class="col-md-3">' +
+'<div class="panel panel-{panelColour} departure-board">' +
+'<div class="panel-heading">' +
+'<h3 class="panel-title">{headingText}</h3>' +
+'</div>' +
+'<ul class="list-group">' +
+'<li class="list-group-item">' +
+'<label>Destination:</label>' +
+'<span>{destination}</span>' +
+'</li>' +
+'<li class="list-group-item">' +
+'<label>Due:</label>' +
+'<span>{due}</span>' +
+'</li>' +
+'<li class="list-group-item">' +
+'<label>Expected:</label>' +
+'<span>{expected}</span>' +
+'</li>' +
+'<li class="list-group-item">' +
+'<label>Operator:</label>' +
+'<span>{operator}</span>' +
+'</li>' +
+'</ul>' + 
+'</div>' +
+'</div>';
+
+var warningIcon = 'Warning <i class="fa fa-exclamation-circle" aria-hidden="true"></i>';
+var alertIcon = 'Alert <i class="fa fa-exclamation-circle" aria-hidden="true"></i>';
+var allGoodIcon = 'All good! <i class="fa fa-check-circle-o" aria-hidden="true"></i>';
+
 // Write your Javascript code.
 $(document).ready(function () {
     $('#station').autocomplete({
@@ -31,14 +61,24 @@ function submitStation() {
         var departureBoard = "";
 
         $.each(data.departureModels, function (index, value){
-            var departure = '<div class="col-md-3"><label>Cancellation reason:</label><span>' + value.cancellationReason + '</span>' + 
-            '<label>Delay reason:</label><span>' + value.delayReason + '</span>' + 
-            '<label>Destination:</label><span>' + value.destinationName + '</span>' + 
-            '<label>Due:</label><span>' + value.due + '</span>' + 
-            '<label>Expected:</label><span>' + value.expected + '</span>' + 
-            '<label>Operator:</label><span>' + value.operator + '</span></div>'
+            var cancellationReasonPresent = value.cancellationReason == null ? false : true;
+            var delayReasonPresent = value.delayReason == null ? false : true;
+            var trainOnTime = value.expected == 'On time' ? true : false;
+            var boardPanel = '';
 
-            departureSection.append(departure);
+            if (trainOnTime) {
+                boardPanel = departureBoardHtml.replace('{panelColour}', 'success').replace('{headingText}', allGoodIcon);
+            }
+            else if (cancellationReasonPresent || delayReasonPresent) {
+                boardPanel = departureBoardHtml.replace('{panelColour}', 'danger').replace('{headingText}', warningIcon);
+            }
+            else {
+                boardPanel = departureBoardHtml.replace('{panelColour}', 'warning').replace('{headingText}', alertIcon);
+            }
+            
+            var departureBoard = boardPanel.replace('{destination}', value.destinationName).replace('{due}', value.due).replace('{expected}', value.expected).replace('{operator}', value.operator);
+
+            departureSection.append(departureBoard);
         })
     });
 }
